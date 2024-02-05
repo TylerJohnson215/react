234b8416e072da69b038483f89076e0306af15e3
Tyler Johnsone <tyler@teambuildr.com>
2024-02-05 10:00:29 -0500

    Adding / learning state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+import React from 'react';
 import './App.css';
 import Employee from './Components/Employee';
+import {useState} from 'react';
 
 function App() {
+  const [role, setRole] = useState('dev');
   const showEmployees = true;
   return (
     <div className="App">
       {showEmployees ? (
       <>
+      <input type='text' onChange={(e) => {
+          setRole(e.target.value)
+      }}
+        />
           <Employee name='Miley' role='Manager'/>
-          <Employee name='Bailey'/>
+          <Employee name='Bailey' role={role}/>
       </>
       ) : (
         <p>You cannot see the employees</p>
