bc5481024c95a5b3cb146ac2773a9ee199aea3e7
Tyler Johnsone <tyler@teambuildr.com>
2024-02-07 20:34:36 -0500

    Updated parent component

--- src/Components/EditEmployee.js
+++ src/Components/EditEmployee.js
@@ -1,8 +1,12 @@
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
+import { propTypes } from 'react-bootstrap/esm/Image';
 import Modal from 'react-bootstrap/Modal';
 
-function EditEmployee() {
+function EditEmployee(props) {
+  const [name, setName] = useState(props.name);
+  const [role, setRole] = useState(props.role);
+
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
@@ -19,7 +23,12 @@ function EditEmployee() {
           <Modal.Title>Update Employee</Modal.Title>
         </Modal.Header>
           <Modal.Body>
-          <form id='editmodal' class="w-full max-w-sm">
+          <form onSubmit={(e) => {
+            handleClose();
+            e.preventDefault();
+            props.updateEmployee(props.id, name, role)
+          }}
+            id='editmodal' class="w-full max-w-sm">
               <div class="md:flex md:items-center mb-6">
                 <div class="md:w-1/3">
                   <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" for="name">
@@ -30,7 +39,9 @@ function EditEmployee() {
                 <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" 
                   id="name" 
                   type="text" 
-                  value="Jane Doe" />
+                  value={name}
+                  onChange={(e)=> {setName(e.target.value)}}
+                   />
               </div>
              </div>
              <div class="md:flex md:items-center mb-6">
@@ -43,7 +54,9 @@ function EditEmployee() {
                 <input class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" 
                   id="role" 
                   type="text" 
-                  value="Jane Doe" />
+                  value={role}
+                  onChange={(e)=> {setRole(e.target.value)}}
+                   />
               </div>
              </div>
              <div class="md:flex md:items-center">
